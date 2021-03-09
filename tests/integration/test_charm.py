5bbea77b88fb4934b246ba97c2dcfbc258281fc3
Dominik Fleischmann <dominik.fleischmann@canonical.com>
2021-03-09 14:38:23 +0100

    Convert to Operator Framework
    This commit contains:
    
        Conversion to Operator Framework
        Unit Tests
        Functional Tests (Not enabled yet)
        GH Action

---
+++ tests/integration/test_charm.py
@@ -0,0 +1,26 @@
+import logging
+from pathlib import Path
+
+import yaml
+
+import pytest
+
+log = logging.getLogger(__name__)
+
+
+@pytest.mark.abort_on_fail
+async def test_build_and_deploy(ops_test):
+    my_charm = await ops_test.build_charm(".")
+    resources = {"oci-image": "quay.io/dexidp/dex:v2.22.0"}
+    await ops_test.model.deploy(my_charm, resources=resources)
+    await ops_test.model.wait_for_idle()
+
+
+async def test_status(ops_test):
+    metadata = Path("./metadata.yaml")
+    charm_name = yaml.safe_load(metadata.read_text())["name"]
+    assert ops_test.model.applications[charm_name].units[0].workload_status == "active"
+    assert (
+        ops_test.model.applications[charm_name].units[0].workload_status_message
+        == "Active and running"
+    )
---
+++ tests/unit/test_charm.py
@@ -0,0 +1,104 @@
+import pytest
+from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
+from ops.testing import Harness
+from unittest.mock import patch
+import yaml
+
+from charm import Operator
+
+
+@pytest.fixture
+def harness():
+    return Harness(Operator)
+
+
+def test_not_leader(harness):
+    harness.begin()
+    assert isinstance(harness.charm.model.unit.status, WaitingStatus)
+
+
+def test_missing_image(harness):
+    harness.set_leader(True)
+    harness.begin_with_initial_hooks()
+    assert isinstance(harness.charm.model.unit.status, BlockedStatus)
+
+
+@patch("bcrypt.hashpw")
+def test_main_no_relation(mock_pw, harness):
+    mock_pw.return_value = "".encode("utf-8")
+    harness.set_leader(True)
+    harness.add_oci_resource(
+        "oci-image",
+        {
+            "registrypath": "quay.io/dexidp/dex:v2.22.0",
+            "username": "",
+            "password": "",
+        },
+    )
+    harness.begin_with_initial_hooks()
+    pod_spec = harness.get_pod_spec()
+
+    # confirm that we can serialize the pod spec
+    yaml.safe_dump(pod_spec)
+
+    assert isinstance(harness.charm.model.unit.status, ActiveStatus)
+
+
+@patch("bcrypt.hashpw")
+def test_main_oidc(mock_pw, harness):
+    mock_pw.return_value = "".encode("utf-8")
+    harness.set_leader(True)
+    harness.add_oci_resource(
+        "oci-image",
+        {
+            "registrypath": "quay.io/dexidp/dex:v2.22.0",
+            "username": "",
+            "password": "",
+        },
+    )
+    rel_id = harness.add_relation("oidc-client", "app")
+
+    harness.begin_with_initial_hooks()
+    harness.add_relation_unit(rel_id, "app/0")
+    data = {
+        "data": "data",
+    }
+    harness.update_relation_data(
+        rel_id,
+        "app/0",
+        data,
+    )
+    assert isinstance(harness.charm.model.unit.status, ActiveStatus)
+    pod_spec = harness.get_pod_spec()
+    config_yaml = pod_spec[0]["containers"][0]["volumeConfig"][0]["files"][0]["content"]
+
+    assert data == yaml.safe_load(config_yaml)["staticClients"][0]
+
+
+@patch("bcrypt.hashpw")
+def test_main_service_mesh(mock_pw, harness):
+    mock_pw.return_value = "".encode("utf-8")
+    harness.set_leader(True)
+    harness.add_oci_resource(
+        "oci-image",
+        {
+            "registrypath": "quay.io/dexidp/dex:v2.22.0",
+            "username": "",
+            "password": "",
+        },
+    )
+    rel_id = harness.add_relation("service-mesh", "app")
+    harness.begin_with_initial_hooks()
+    harness.add_relation_unit(rel_id, "app/0")
+    assert isinstance(harness.charm.model.unit.status, ActiveStatus)
+    relation_data = harness.get_relation_data(rel_id, harness.charm.app.name)
+    data = {
+        "auth": {},
+        "ingress": False,
+        "port": 5556,
+        "prefix": "/dex",
+        "rewrite": "/dex",
+        "service": "dex-auth",
+    }
+
+    assert data == yaml.safe_load(relation_data["data"])
