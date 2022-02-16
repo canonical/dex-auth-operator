70751dd4cb9ad4eed65e0636cbddbe1b272bec55
Natasha Ho <natasha.ho@canonical.com>
2022-02-16 11:39:46 -0500

    add istio-gateway

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -29,34 +29,46 @@ async def test_status(ops_test):
 async def test_access_login_page(ops_test):
     oidc = "oidc-gatekeeper"
     istio = "istio-pilot"
+    istio_gateway = "istio-gateway"
     dex = METADATA["name"]
-    status = await ops_test.model.get_status()
-    dex_ip = status["applications"][dex]["public-address"]
+
     oidc_config = {
         "client-id": "authservice-oidc",
         "client-name": "Ambassador Auth OIDC",
         "client-secret": "oidc-client-secret",
         "oidc-scopes": "openid profile email groups",
-        "public-url": dex_ip,
     }
     await ops_test.model.deploy(oidc, config=oidc_config)
     await ops_test.model.deploy(istio, channel="1.5/stable")
+    await ops_test.model.deploy(istio_gateway, channel="1.5/stable", trust=True)
     await ops_test.model.add_relation(oidc, dex)
     await ops_test.model.add_relation(f"{istio}:ingress", f"{dex}:ingress")
     await ops_test.model.add_relation(f"{istio}:ingress", f"{oidc}:ingress")
     await ops_test.model.add_relation(f"{istio}:ingress-auth", f"{oidc}:ingress-auth")
+    await ops_test.model.add_relation(istio, istio_gateway)
 
     await ops_test.model.wait_for_idle(
-        [dex, oidc, istio],
+        [dex, oidc, istio, istio_gateway],
+        status="active",
+        timeout=600,
+    )
+
+    status = await ops_test.model.get_status()
+    istio_gateway_ip = status["applications"][istio_gateway]["public-address"]
+    await ops_test.model.applications[oidc].set_config({"public-url": istio_gateway_ip})
+
+    await ops_test.model.wait_for_idle(
+        [dex, oidc, istio, istio_gateway],
         status="active",
         raise_on_blocked=False,
         # oidc transient errors when update public url
+        # https://github.com/canonical/oidc-gatekeeper-operator/issues/21
         raise_on_error=False,
         timeout=600,
     )
 
     url = (
-        f"http://{dex_ip}:5556/dex/auth?client_id={oidc_config['client-id']}"
+        f"http://{istio_gateway_ip}/dex/auth?client_id={oidc_config['client-id']}"
         f"&redirect_uri=%2Fauthservice%2Foidc%2Fcallback&response_type=code"
         f"&scope={oidc_config['oidc-scopes'].replace(' ', '+')}&state="
     )
