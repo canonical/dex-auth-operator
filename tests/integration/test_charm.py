65c51e86ae456925d5ecf6fac9f9980024c5a936
Natasha Ho <natasha.ho@canonical.com>
2022-02-18 15:57:58 -0500

    set public-urls at deploy

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -12,6 +12,7 @@ from lightkube.models.rbac_v1 import PolicyRule
 log = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+ISTIO_GATEWAY_ADDRESS = "http://10.64.140.43.nip.io"
 
 
 @pytest.mark.abort_on_fail
@@ -19,7 +20,9 @@ async def test_build_and_deploy(ops_test):
     my_charm = await ops_test.build_charm(".")
     image_path = METADATA["resources"]["oci-image"]["upstream-source"]
     resources = {"oci-image": image_path}
-    await ops_test.model.deploy(my_charm, resources=resources)
+    await ops_test.model.deploy(
+        my_charm, resources=resources, config={"public-url": ISTIO_GATEWAY_ADDRESS}
+    )
     await ops_test.model.wait_for_idle()
 
 
@@ -40,6 +43,7 @@ async def test_access_login_page(ops_test):
         "client-name": "Ambassador Auth OIDC",
         "client-secret": "oidc-client-secret",
         "oidc-scopes": "openid profile email groups",
+        "public-url": ISTIO_GATEWAY_ADDRESS,
     }
     await ops_test.model.deploy(oidc, config=oidc_config)
     await ops_test.model.deploy(istio, channel="1.5/stable")
@@ -68,23 +72,6 @@ async def test_access_login_page(ops_test):
     lightkube_client.patch(Role, istio_gateway_role_name, this_role)
 
     await ops_test.model.set_config({"update-status-hook-interval": "1m"})
-
-    await ops_test.model.wait_for_idle(
-        [dex, oidc, istio, istio_gateway],
-        status="active",
-        timeout=700,
-    )
-
-    status = await ops_test.model.get_status()
-    istio_gateway_address = (
-        "http://" + status["applications"][istio_gateway]["public-address"] + ".nip.io"
-    )
-    await ops_test.model.applications[dex].set_config(
-        {"public-url": istio_gateway_address}
-    )
-    await ops_test.model.applications[oidc].set_config(
-        {"public-url": istio_gateway_address}
-    )
 
     await ops_test.model.wait_for_idle(
         [dex, oidc, istio, istio_gateway],
