a32553e4330ac7dea270849181ce44d1044f2270
Natasha Ho <natasha.ho@canonical.com>
2022-03-04 11:09:10 +0100

    run integration test as microk8s

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -9,6 +9,8 @@ from lightkube.core.client import Client
 from lightkube.resources.rbac_authorization_v1 import Role
 from lightkube.models.rbac_v1 import PolicyRule
 import time
+from lightkube.config.kubeconfig import KubeConfig
+from pytest_operator.plugin import OpsTest
 
 log = logging.getLogger(__name__)
 
@@ -27,7 +29,7 @@ async def test_build_and_deploy(ops_test):
     image_path = METADATA["resources"]["oci-image"]["upstream-source"]
     resources = {"oci-image": image_path}
     await ops_test.model.deploy(my_charm, resources=resources, config=DEX_CONFIG)
-    await ops_test.model.wait_for_idle()
+    await ops_test.model.wait_for_idle(status="active")
 
 
 async def test_status(ops_test):
@@ -37,6 +39,7 @@ async def test_status(ops_test):
 
 @pytest.mark.abort_on_fail
 async def test_access_login_page(ops_test):
+    # ops_test.keep_model = True
     oidc = "oidc-gatekeeper"
     istio = "istio-pilot"
     istio_gateway = "istio-gateway"
@@ -65,6 +68,9 @@ async def test_access_login_page(ops_test):
     )
 
     lightkube_client = Client(
+        config=KubeConfig.from_file(
+            "/var/snap/microk8s/current/credentials/client.config"
+        ),
         namespace=ops_test.model_name,
     )
 
