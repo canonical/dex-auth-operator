b43eb5a737f0055f620f4cb396ac9d2dc003f04a
Chris Johnston <chris.johnston@canonical.com>
2022-06-08 03:16:14 +0000

    trust prometheus and grafana in integration tests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -64,8 +64,8 @@ async def test_prometheus_grafana_integration(ops_test: OpsTest):
     prometheus_scrape_charm = "prometheus-scrape-config-k8s"
     scrape_config = {"scrape_interval": "5s"}
 
-    await ops_test.model.deploy(prometheus, channel="latest/beta")
-    await ops_test.model.deploy(grafana, channel="latest/beta")
+    await ops_test.model.deploy(prometheus, channel="latest/beta", trust=True)
+    await ops_test.model.deploy(grafana, channel="latest/beta", trust=True)
     await ops_test.model.add_relation(prometheus, grafana)
     await ops_test.model.add_relation(APP_NAME, grafana)
     await ops_test.model.deploy(
