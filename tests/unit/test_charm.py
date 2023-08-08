fc6c7e72f63246a804e6d208a22696de3a53bd14
Daniela Plascencia <daniela.plascencia@canonical.com>
2023-08-08 12:00:24 +0200

    skip: set can connect order
    Co-authored-by: Phoevos Kalemkeris <phoevos.kalemkeris@canonical.com>

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -43,8 +43,8 @@ def ensure_state(self):
 @patch("charm.Operator._update_layer")
 def test_install_event(update, harness):
     harness.set_leader(True)
-    harness.set_can_connect("dex", True)
     harness.begin()
+    harness.set_can_connect("dex", True)
 
     harness.charm.on.install.emit()
     update.assert_called()
