6fe023dc07ee5b6df7c33dc86bccb1e424d156e7
Daniela Plascencia <daniela.plascencia@canonical.com>
2024-09-18 23:58:39 +0200

    feat: add Terraform module for dex-auth
    This commit adds the terraform/ directory to the root of the repository to host
    the Terraform module of this charm. This follows the standard set in CC006.
    For more information please also refer to canonical/argo-operators/pull/198.
    
    Fixes #226

---
+++ terraform/outputs.tf
@@ -0,0 +1,18 @@
+output "app_name" {
+  value = juju_application.dex_auth.name
+}
+
+output "provides" {
+  value = {
+    dex_oidc_config = "dex-oidc-config",
+    grafana_dashboard = "grafana-dashboard",
+    metrics_endpoint  = "metrics-endpoint",
+  }
+}
+
+output "requires" {
+  value = {
+    ingress = "ingress",
+    oidc_client = "oidc-client",
+  }
+}
