0363f5bf962eb5b95db5f487e166fdc98fde2652
NohaIhab <noha.ihab@canonical.com>
2026-04-27 20:10:35 +0300

    fix(tests): update channels of dependencies to track/edge

--- tests/integration/charms_dependencies.py
+++ tests/integration/charms_dependencies.py
@@ -4,7 +4,7 @@ from charmed_kubeflow_chisme.testing import CharmSpec
 
 OIDC_GATEKEEPER = CharmSpec(
     charm="oidc-gatekeeper",
-    channel="ckf-1.10/stable",
+    channel="ckf-1.10/edge",
     config={
         "client-name": "Ambassador Auth OIDC",
         "client-secret": "oidc-client-secret",
@@ -12,13 +12,13 @@ OIDC_GATEKEEPER = CharmSpec(
     trust=True,
 )
 ISTIO_GATEWAY = CharmSpec(
-    charm="istio-gateway", channel="1.24/stable", trust=True, config={"kind": "ingress"}
+    charm="istio-gateway", channel="1.24/edge", trust=True, config={"kind": "ingress"}
 )
 ISTIO_PILOT = CharmSpec(
     charm="istio-pilot",
-    channel="1.24/stable",
+    channel="1.24/edge",
     trust=True,
     config={"default-gateway": "kubeflow-gateway"},
 )
-KUBEFLOW_DASHBOARD = CharmSpec(charm="kubeflow-dashboard", channel="1.10/stable", trust=True)
-KUBEFLOW_PROFILES = CharmSpec(charm="kubeflow-profiles", channel="1.10/stable", trust=True)
+KUBEFLOW_DASHBOARD = CharmSpec(charm="kubeflow-dashboard", channel="2.0/edge", trust=True)
+KUBEFLOW_PROFILES = CharmSpec(charm="kubeflow-profiles", channel="2.0/edge", trust=True)
